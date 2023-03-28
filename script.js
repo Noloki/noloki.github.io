91dcafe6a9787e1ef13a18e22ae8b757c64b15f8
Noloki <68790261+Noloki@users.noreply.github.com>
2023-03-28 14:00:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', () =>
-  requestAnimationFrame(updateTime)
-)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', () =>
+    requestAnimationFrame(updateTime)
+  )
 
-function updateTime() {
-  document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
-  document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
-  document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
-  document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
-  requestAnimationFrame(updateTime);
-}
+  function updateTime() {
+    document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
+    document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
+    document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
+    document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
+    requestAnimationFrame(updateTime);
+  }
+</script>
